574a2beabf3d6758347fb4b48438a6e18bc50e5b
Jos Dehaes <jos.dehaes@gmail.com>
2025-11-05 15:39:48 +0100

    fix: disable verify_mutants

--- move-mutation-test/src/cli.rs
+++ move-mutation-test/src/cli.rs
@@ -83,7 +83,7 @@ pub fn create_mutator_options(
         downsampling_ratio_percentage: options.downsampling_ratio_percentage,
         apply_coverage,
         // To run tests, compilation must succeed
-        verify_mutants: true,
+        verify_mutants: false,
         mode: options.mode,
         operators: options.operators.clone(),
         ..Default::default()
